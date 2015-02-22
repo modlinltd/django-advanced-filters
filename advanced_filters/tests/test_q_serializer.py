87208daa99b282246a2b0a8d22d8ef8958b77cff
Pavel Savchenko <asfaltboy@gmail.com>
2015-02-22 15:04:58 +0100

    tests: move tests into the app and rewrite to support other versions
    support Django == 1.5 AND >= 1.6, support python 3

--- advanced_filters/tests/test_q_serializer.py
+++ advanced_filters/tests/test_q_serializer.py
@@ -1,10 +1,24 @@
 from django.db.models import Q
 from django.test import TestCase
+import django
+
+import simplejson as json
 
 from advanced_filters.q_serializer import QSerializer
 
+NEWER_DJANGO = django.VERSION >= (1, 6)
+
 
 class QSerializerTest(TestCase):
+    correct_query = {
+        'children': [('test', 1234)],
+        'connector': u'AND',
+        'negated': False,
+    }
+    if not NEWER_DJANGO:
+        correct_query['subtree_parents'] = []
+
+    correct_query_json = json.dumps(correct_query)
 
     def setUp(self):
         self.s = QSerializer()
@@ -13,20 +27,14 @@ class QSerializerTest(TestCase):
 
     def test_serialize_q(self):
         res = self.s.serialize(self.query_a)
-        self.assertEquals(
-            res, {
-                'children': [('test', 1234)],
-                'connector': u'AND',
-                'negated': False,
-            }
-        )
+
+        self.assertEquals(res, self.correct_query)
 
         jres = self.s.dumps(self.query_a)
-        self.assertEquals(
-            jres,
-            '{"connector": "AND", "negated": false, "children": [["test", '
-            '1234]]}'
-        )
+        self.assertEquals(jres, self.correct_query_json)
+        # '{"connector": "AND", "negated": false, "children": [["test", '
+        # '1234]], "subtree_parents": []}'
+        # )
 
     def test_deserialize_q(self):
         qres = self.s.deserialize({
