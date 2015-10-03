6b77953070822867fbcb14e64fdb07ff0ab06534
Pavel Savchenko <pavel@modlinltd.com>
2015-10-03 19:44:21 +0200

    fix(views): resolve settings inside view and refine error handling

--- advanced_filters/views.py
+++ advanced_filters/views.py
@@ -1,11 +1,11 @@
 from operator import itemgetter
-from pprint import pformat
 import logging
 
 from django.conf import settings
 from django.contrib.admin.util import get_fields_from_path
 from django.db import models
 from django.db.models.fields import FieldDoesNotExist
+from django.utils.encoding import force_text
 from django.views.generic import View
 
 from braces.views import (CsrfExemptMixin, StaffuserRequiredMixin,
@@ -13,41 +13,42 @@ from braces.views import (CsrfExemptMixin, StaffuserRequiredMixin,
 
 logger = logging.getLogger('advanced_filters.views')
 
-DISABLE_FOR_FIELDS = getattr(settings, 'ADVANCED_FILTERS_DISABLE_FOR_FIELDS',
-                             tuple())
-MAX_CHOICES = getattr(settings, 'ADVANCED_FILTERS_MAX_CHOICES', 254)
-
 
 class GetFieldChoices(CsrfExemptMixin, StaffuserRequiredMixin,
                       JSONResponseMixin, View):
     """
     A JSONResponse view that accepts a model and a field (path to field),
     resolves and returns the valid choices for that field.
+    Model must use the "app.Model" notation.
 
     If this field is not a simple Integer/CharField with predefined choices,
     all distinct entries in the DB are presented, unless field name is in
-    DISABLE_FOR_FIELDS and limited to a maximum of MAX_CHOICES.
+    ADVANCED_FILTERS_DISABLE_FOR_FIELDS and limited to display only results
+    under ADVANCED_FILTERS_MAX_CHOICES.
     """
-    def get(self, request, model, field_name):
-        app_label, model_name = model.split('.', 1)
-        model_obj = models.get_model(app_label, model_name)
-        if not model_obj:
+    def get(self, request, model=None, field_name=None):
+        if model is field_name is None:
             return self.render_json_response(
-                {'error': 'No model found for %s' % model}, status=400)
-
+                {'error': "GetFieldChoices view requires 2 arguments"},
+                status=400)
+        app_label, model_name = model.split('.', 1)
         try:
+            model_obj = models.get_model(app_label, model_name)
             field = get_fields_from_path(model_obj, field_name)[-1]
             model_obj = field.model  # use new model if followed a ForeignKey
-        except FieldDoesNotExist:
+        except (LookupError, FieldDoesNotExist) as e:
+            logger.debug("Invalid kwargs passed to view: %s", e)
             return self.render_json_response(
-                {'error': 'No such field %s found in model %s' %
-                    (field_name, model)}, status=400)
+                {'error': force_text(e)}, status=400)
 
         choices = field.choices
         # if no choices, populate with distinct values from instances
         if not choices:
             choices = []
-            if field.name in DISABLE_FOR_FIELDS:
+            disabled = getattr(settings, 'ADVANCED_FILTERS_DISABLE_FOR_FIELDS',
+                               tuple())
+            max_choices = getattr(settings, 'ADVANCED_FILTERS_MAX_CHOICES', 254)
+            if field.name in disabled:
                 logger.debug('Skipped lookup of choices for disabled fields')
             elif isinstance(field, (models.BooleanField, models.DateField,
                                     models.TimeField)):
@@ -59,7 +60,7 @@ class GetFieldChoices(CsrfExemptMixin, StaffuserRequiredMixin,
                 if len(choices) < MAX_CHOICES:
                     choices = zip(choices, choices)
                     logger.debug('Choices found for field %s: %s',
-                                 field.name, pformat(choices))
+                                 field.name, choices)
                 else:
                     choices = []
 
