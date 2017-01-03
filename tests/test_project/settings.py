200f3141daa818e09ce9d74e446bb12ce3510889
Pavel Savchenko <asfaltboy@gmail.com>
2017-01-03 19:02:34 +0100

    feat: test with both Python 3.5 and Django 1.10
    Update matrix to support all django versions for Python 3.5 and
    all python versions for Django 1.10

--- tests/test_project/settings.py
+++ tests/test_project/settings.py
@@ -54,6 +54,12 @@ ROOT_URLCONF = 'tests.test_project.urls'
 WSGI_APPLICATION = 'tests.test_project.wsgi.application'
 
 AUTH_USER_MODEL = 'reps.SalesRep'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+    },
+]
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
