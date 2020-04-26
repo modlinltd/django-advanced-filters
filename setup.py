ecfdf417f64dadf658b9862fe26b597b1ed96d45
Pavel Savchenko <asfaltboy@gmail.com>
2020-04-26 20:23:51 +0200

    chore: update python and add Django classifiers

--- setup.py
+++ setup.py
@@ -81,8 +81,18 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Framework :: Django',
+        'Framework :: Django :: 1.9'
+        'Framework :: Django :: 1.10'
+        'Framework :: Django :: 1.11'
+        'Framework :: Django :: 2.0'
+        'Framework :: Django :: 2.1'
+        'Framework :: Django :: 2.2'
+        'Framework :: Django :: 3.0'
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
